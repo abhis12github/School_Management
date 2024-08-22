edbe82412498dbd61254ec261001f66922ef7d93
Abhishek Anand <mailtomeabhi15@gmail.com>
2024-08-22 21:02:37 +0530

    Added service layer and logic to sort the schools on basis of proximity.

---
+++ src/services/school-service.js
@@ -0,0 +1,38 @@
+const SchoolRepository=require("../repository/school-repository");
+const {calculateDistance}=require("../utils/coordinates-distance");
+
+class SchoolService{
+    constructor(){
+        this.schoolRepository=new SchoolRepository();
+    }
+
+    async createSchool(data){
+        try {
+            const school=await this.schoolRepository.createSchool(data);
+            return school;
+        } catch (error) {
+            console.log("Something went wrong in the school service layer");
+            throw {error};
+        }   
+    }
+
+    async getAllSchools(coordinates){
+        try {
+            const schools=await this.schoolRepository.getAllSchools();
+
+            const sortedSchools = schools.sort((schoolA, schoolB) => {
+                const distanceA = calculateDistance(coordinates.latitude, coordinates.longitude, schoolA.latitude, schoolA.longitude);
+                const distanceB = calculateDistance(coordinates.latitude, coordinates.longitude, schoolB.latitude, schoolB.longitude);
+                return distanceA - distanceB;
+            });
+
+            return sortedSchools;
+
+        } catch (error) {
+            console.log("Something went wrong in the school service layer");
+            throw {error};    
+        }
+    }
+}
+
+module.exports=SchoolService;
---
+++ src/utils/coordinates-distance.js
@@ -0,0 +1,20 @@
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+//Distance calculation using Haversine Formula.
+function calculateDistance(latitude1, longitude1, latitude2, longitude2) {
+    const radius = 6371;
+    const latitudeDiff = degreesToRadians(latitude2 - latitude1);
+    const longitudeDiff = degreesToRadians(longitude2 - longitude1);
+    const a = Math.sin(latitudeDiff / 2) * Math.sin(latitudeDiff / 2) +
+        Math.cos(degreesToRadians(latitude1)) * Math.cos(degreesToRadians(latitude2)) *
+        Math.sin(longitudeDiff / 2) * Math.sin(longitudeDiff / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = radius * c;
+    return distance;
+}
+
+module.exports = {
+    calculateDistance
+};
