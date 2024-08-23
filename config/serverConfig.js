46f597796ad675465d7391a10991341b745c54ee
Abhishek Anand <mailtomeabhi15@gmail.com>
2024-08-23 17:29:58 +0530

    Added db sync logic

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -8,5 +8,6 @@ module.exports={
     DB_PASSWORD:process.env.DB_PASSWORD,
     DB_HOST:process.env.DB_HOST,
     DB_NAME:process.env.DB_NAME,
-    NODE_ENV:process.env.NODE_ENV
+    NODE_ENV:process.env.NODE_ENV,
+    DB_SYNC:process.env.DB_SYNC
 };
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const {PORT}=require("./config/serverConfig");
+const { PORT } = require("./config/serverConfig");
 
-const ApiRoutes=require("./routes/index");
+const ApiRoutes = require("./routes/index");
 
-const setupAndStartServer=async()=>{ 
+const db = require("./models/index");
+const { DB_SYNC } = require("./config/serverConfig");
 
-    const app=express();
+if (DB_SYNC) {
+    db.sequelize.sync({ alter: true });
+}
+
+const setupAndStartServer = async () => {
+
+    const app = express();
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use("/api",ApiRoutes);
-    
-    app.listen(PORT,async()=>{
+    app.use("/api", ApiRoutes);
+
+    app.listen(PORT, async () => {
         console.log(`Server started on port ${PORT}`);
     });
 }
